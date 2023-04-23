0301d09126b6d1c7dffc4e2a5b22fa94e79b8acb
TakoMG <127013101+Migueltak@users.noreply.github.com>
2023-04-23 02:36:50 -0600

    Update Ev2.py

--- Ev2.py
+++ Ev2.py
@@ -12,7 +12,7 @@ resultado = 0
 TITULO = ""
 columnas = ("TITULO", "AUTOR", "GENERO", "AÑO DE PUBLICACION", "FECHA DE ADQUISICIÓN", "CANTIDAD", "ISBN")
 ISBN = []
-registro = []
+registros = []
 AÑO_DE_PUBLICION = 
 
 def menu():
