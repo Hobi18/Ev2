bec108a6af334a8b99e7a61da2caaff8710ff484
TakoMG <127013101+Migueltak@users.noreply.github.com>
2023-04-23 19:10:38 -0600

    Update Ev2.py

--- Ev2.py
+++ Ev2.py
@@ -3,13 +3,8 @@ import os
 import datetime
 
 respuesta = 1
-CANTIDAD = 0
-GENERO = ""
-AUTOR = ""
-resultado = 0
-TITULO = ""
 columnas = ("TITULO", "AUTOR", "GENERO", "AÑO DE PUBLICACION", "FECHA DE ADQUISICIÓN", "CANTIDAD", "ISBN")
-registros = []
+libros = []
 
 def menu():
     print("\n BIBLIOTECA ")
@@ -18,16 +13,16 @@ def menu():
     print("2. CONSULTAS")
     print("3. REPORTES (SEGUN EL AÑO)")
     print("4. SALIR.")
-    
+
 def CSV_A_Lista():
     ruta = os.path.abspath(os.getcwd())
     archivo_trabajo = ruta + "\\datos.csv"
     if os.path.exists(archivo_trabajo):
         with open("datos.csv", "r") as archivo: 
             lector = csv.reader(archivo, delimiter=',')
-            registros = []
-            for row in lector:
-                registros.append(row)
+            next(lector)  # Saltar la fila de encabezados
+            for fila in lector:
+                libros.append(tuple(fila))
         archivo.close()
     else:
         with open("datos.csv", "w", newline="") as archivo:
@@ -35,36 +30,31 @@ def CSV_A_Lista():
             registrador.writerow(columnas)
             archivo.close()
 
-    return registros
-
-def Lista_A_CSV(registros):
+def Lista_A_CSV():
     ruta = os.path.abspath(os.getcwd())
     archivo_trabajo = ruta + "\\datos.csv"
     if os.path.exists(archivo_trabajo):
         with open("datos.csv", "w", newline="") as archivo: 
             registrador = csv.writer(archivo)
             registrador.writerow(columnas)
-            registrador.writerows(registros)
+            registrador.writerows(libros)
         archivo.close()
 
-registros = CSV_A_Lista()
+CSV_A_Lista()
 
-while (True):
+while True:
     menu()
     op = input("¿Qué opción deseas?: ")
-    
     if op == "1":
         while respuesta == 1:
             registro = []
-
             TITULO = input("\nIngresa el titulo del libro: ")
             AUTOR = input("\nIngresa el autor del libro: ")
             GENERO = input("\nIngresa el GENERO del Libro: ")
-            AÑO_DE_PUBLICACION = input("\nIngresa el año de su publicacion (dd/mm/aaaa): ")
+            AÑO_DE_PUBLICACION = input("\nIngresa el año de su publicacion: ")
             FECHADEADQUISICION = input("\nIngresa la fecha de adquisicion (dd/mm/aaaa): ")
             CANTIDAD = int(input("\nIngresa la cantidad de libros que deseas adquirir: "))
-            ISBN = int(input("\nIngresa el ISBN del libro: "))
-
+            ISBN = input("\nIngresa el ISBN del libro: ")
             registro.append(TITULO)
             registro.append(AUTOR)
             registro.append(GENERO)
@@ -72,8 +62,9 @@ while (True):
             registro.append(FECHADEADQUISICION)
             registro.append(CANTIDAD)
             registro.append(ISBN)
-            registros.append(registro)
-            Lista_A_CSV(registros)
-
+            libros.append(tuple(registro))
             respuesta = int(input("\n¿Deseas capturar otro registro? \n (1.SI - 0.NO): "))
-        respuesta = 1
+    elif op == "4":
+        break
+
+    Lista_A_CSV()
